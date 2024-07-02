79aa79b7720916750b079c6783a5f0658610c0cb
Abhisekh26 <142305216+Abhisekh26@users.noreply.github.com>
2024-07-02 13:02:25 +0530

    took first last emaila nd password from user for sign up page

--- src/app/(registration)/signup/page.js
+++ src/app/(registration)/signup/page.js
@@ -1,6 +1,23 @@
+"use client";
 import Link from "next/link";
+import { useRef } from "react";
 
 export default function Signup() {
+  const firstref = useRef();
+  const lastref = useRef();
+  const emailref = useRef();
+  const passref = useRef();
+
+  function siggnup(event) {
+    event.preventDefault();
+    console.log("hello");
+    const firstname = firstref.current.value;
+    const lastname = lastref.current.value;
+    const email = emailref.current.value;
+    const password = passref.current.value;
+    console.log(firstname,lastname,email,password);
+  }
+
   return (
     <div>
       <div class="flex flex-col items-center justify-center dark mt-6 mb-16">
@@ -12,23 +29,27 @@ export default function Signup() {
                 placeholder="First Name"
                 class="bg-gray-700 text-gray-200 border-0 rounded-md p-2 w-1/2 focus:bg-gray-600 focus:outline-none focus:ring-1 focus:ring-blue-500 transition ease-in-out duration-150"
                 type="text"
+                ref={firstref}
               />
               <input
                 placeholder="Last Name"
                 class="bg-gray-700 text-gray-200 border-0 rounded-md p-2 w-1/2 focus:bg-gray-600 focus:outline-none focus:ring-1 focus:ring-blue-500 transition ease-in-out duration-150"
                 type="text"
+                ref={lastref}
               />
             </div>
             <input
               placeholder="Email"
               class="bg-gray-700 text-gray-200 border-0 rounded-md p-2 mb-4 focus:bg-gray-600 focus:outline-none focus:ring-1 focus:ring-blue-500 transition ease-in-out duration-150"
               type="email"
+              ref={emailref}
             />
 
             <input
               placeholder="Password"
               class="bg-gray-700 text-gray-200 border-0 rounded-md p-2 mb-4 focus:bg-gray-600 focus:outline-none focus:ring-1 focus:ring-blue-500 transition ease-in-out duration-150"
               type="password"
+              ref={passref}
             />
             <input
               placeholder="Confirm Password"
@@ -44,6 +65,7 @@ export default function Signup() {
             <select
               class="bg-gray-700 text-gray-200 border-0 rounded-md p-2 mb-4 focus:bg-gray-600 focus:outline-none focus:ring-1 focus:ring-blue-500 transition ease-in-out duration-150"
               id="gender"
+              // ref={genderref}
             >
               <option value="male">Male</option>
               <option value="female">Female</option>
@@ -69,6 +91,7 @@ export default function Signup() {
             <button
               class="bg-gradient-to-r from-indigo-500 to-blue-500 text-white font-bold py-2 px-4 rounded-md mt-4 hover:bg-indigo-600 hover:to-blue-600 transition ease-in-out duration-150"
               type="submit"
+              onClick={siggnup}
             >
               Sign Up
             </button>
