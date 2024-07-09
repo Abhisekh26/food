bac038f386c28e193a749f84f20b6065fd22bd94
Abhisekh26 <142305216+Abhisekh26@users.noreply.github.com>
2024-07-09 14:55:52 +0530

    filterslice

--- src/app/reduxStore/filterSlice.js
+++ src/app/reduxStore/filterSlice.js
@@ -4,43 +4,36 @@ const filterData = createSlice({
   name: "filteringData",
   initialState: {
     filteredArray: [],
-    x:null,
+    x: null,
   },
   reducers: {
     displayData(state, actions) {
       state.filteredArray = actions.payload;
     },
 
+    filterPrice(state, actions) {
+      state.x = actions.payload.split("-");
+      const first = parseInt(state.x[0]);
+      const second = parseInt(state.x[1]);
+      state.filteredArray = state.filteredArray.filter(
+        (item) => item.discountPrice > first && item.discountPrice < second
+      );
+    },
 
+    filterRating(state, actions) {
+      const rate = parseInt(actions.payload);
+      state.filteredArray = state.filteredArray.filter(
+        (item) => item.ratings > rate
+      );
+    },
 
-
-   filterPrice(state,actions){
-    state.x=actions.payload.split("-")
-    const first=parseInt( state.x[0])
-    const second=parseInt( state.x[1])
-    state.filteredArray= state.filteredArray.filter((item)=>
-              item.discountPrice > first && item.discountPrice < second
-    )
-     },
-
-   
-      filterRating(state,actions){
-      const rate=(parseInt(actions.payload))
-      state.filteredArray=state.filteredArray.filter((item)=>
-        item.ratings > rate
-      )
-
-      },
-
-      filterBestseller(state,actions){
-        // const best=console.log(actions.payload)
-         actions.payload ?state.filteredArray=state.filteredArray.filter((items)=>
-         items.bestsellers  === "yes"
-         ) :null
-      }
-
-
-
+    filterBestseller(state, actions) {
+      actions.payload
+        ? (state.filteredArray = state.filteredArray.filter(
+            (items) => items.bestsellers === "yes"
+          ))
+        : null;
+    },
   },
 });
 export const filterDataActions = filterData.actions;
