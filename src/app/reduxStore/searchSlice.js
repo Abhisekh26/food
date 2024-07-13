a103215c04e8c6f5cc72612295be8fbf740d53bc
Abhisekh26 <142305216+Abhisekh26@users.noreply.github.com>
2024-07-13 12:44:46 +0530

    completed search slice

--- src/app/reduxStore/searchSlice.js
+++ src/app/reduxStore/searchSlice.js
@@ -3,17 +3,23 @@ const { createSlice } = require("@reduxjs/toolkit");
 const search = createSlice({
   name: "searching",
   initialState: {
-    result: {},
+    result: [],
+    userInput: "",
   },
   reducers: {
     searchResult(state, action) {
       state.result = action.payload;
     },
-    finalResult(state,action){
-      state.result=state.result.filter((item)=>{
-        
-      })
-    }
+
+    searchParameter(state, action) {
+      state.userInput = action.payload;
+    },
+
+    finalResult(state, action) {
+      state.result = state.result.filter((item) => {
+      return   item.title.toLowerCase().includes(state.userInput.toLowerCase());
+      });
+    },
   },
 });
 export const searchActions = search.actions;
