6d7c7f1bb6242353f0f168236a8aae17eb175b8b
Abhisekh26 <142305216+Abhisekh26@users.noreply.github.com>
2024-09-25 15:10:02 +0530

    vercell

---
+++  next.config.js
@@ -0,0 +1,10 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    reactStrictMode: true,
+    images: {
+      domains: ['*'], // Allows all domains (not recommended for production)
+    },
+  };
+  
+  module.exports = nextConfig;
+  
